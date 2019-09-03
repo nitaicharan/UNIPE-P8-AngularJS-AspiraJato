5b5a79b486f0ab9c6005e5380f131970fce57008
Nitai Charan <nitaicharan@gmail.com>
2019-09-03 10:08:30 -0300

    feature#168217049 - Consumir API LUIS
    Iniciado servico para consulta a API LUIZ

--- src/app/searchbox/searchbox.component.js
+++ src/app/searchbox/searchbox.component.js
@@ -1,13 +1,18 @@
 'use strict';
 
-angular.module('ajSearchpage',[])
-    .component('ajSearchpage', {
-        templateUrl: '/app/searchbox/searchbox.component.html',
-        controller: ['$scope',function($scope){
-            console.log($scope.search);
-            oninit();
-            function oninit (){
-                $scope.search = '';
+angular.module('ajSearchpage',['resource']).component('ajSearchpage', {
+    templateUrl: '/app/searchbox/searchbox.component.html',
+    controller: ['$scope','resource',function($scope,resource){
+        oninit();
+
+        $scope.spend = function (){
+            if($scope.search){
+                $scope.rs = resource($scope.search).get();
             }
-        }]
+        }
+
+        function oninit (){
+            $scope.search = "Apagar luz sala e cozinha";
+        }
+    }]
 });
